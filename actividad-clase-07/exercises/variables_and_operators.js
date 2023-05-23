38b017db23ee4a49517ffa5a4ad7f1f9daf648c3
unknown <olivarigabriel@gmail.com>
2023-05-23 19:53:32 -0300

    Clase 08

---
+++ actividad-clase-07/exercises/variables_and_operators.js
@@ -0,0 +1,17 @@
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+const num1 = 1;
+const num2 = 2;
+const sum = num1 + num2;
+console.log(sum);
+
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una3er variable.
+const str1 = "clase";
+const str2 = " 08";
+const concat = str1 + str2;
+console.log(concat);
+
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+const firstName = "Gabriel";
+const lastName = "Olivari";
+const length = firstName.length + lastName.length;
+console.log(length);
