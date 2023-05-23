e462d53123caeb0c6790aa0ee30d959bba1cd0bc
unknown <olivarigabriel@gmail.com>
2023-05-23 20:13:47 -0300

    Agregado ejercicios variables y strings

--- actividad-clase-07/exercises/variables_and_operators.js
+++ actividad-clase-07/exercises/variables_and_operators.js
@@ -1,17 +1,19 @@
+console.log("Ejercicios variables_and_operators")
+
 // a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
-const num1 = 1;
-const num2 = 2;
-const sum = num1 + num2;
-console.log(sum);
+var num1 = 1;
+var num2 = 2;
+var sum = num1 + num2;
+console.log("Ejercicio a:", sum);
 
 // b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una3er variable.
-const str1 = "clase";
-const str2 = " 08";
-const concat = str1 + str2;
-console.log(concat);
+var str1 = "clase";
+var str2 = " 08";
+var concat = str1 + str2;
+console.log("Ejercicio b:", concat);
 
 // c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
-const firstName = "Gabriel";
-const lastName = "Olivari";
-const length = firstName.length + lastName.length;
-console.log(length);
+var firstName = "Gabriel";
+var lastName = "Olivari";
+var length = firstName.length + lastName.length;
+console.log("Ejercicio c:", length);
